b74b752a6dd72d957db2db8da5167c7af1041041
Daniel Steman <daniel-steman@live.nl>
2023-01-23 20:50:29 +0100

    test git creds

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -52,7 +52,6 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   programs.git = {
     enable = true;
@@ -62,7 +61,7 @@
     extraConfig = {
       commit.gpgsign = true;
       gpg.format = "ssh";
-      user.signingkey = "~/.ssh/id_ed25519.pub";
+      user.signingkey = "/home/daniel/.ssh/id_ed25519.pub";
     };
   };
 
