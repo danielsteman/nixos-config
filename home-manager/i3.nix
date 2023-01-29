a93fec503feab5ee2478d144d56d8e342da0a612
Daniel Steman <daniel-steman@live.nl>
2023-01-29 19:17:04 +0100

    fix modifier key

--- home-manager/i3.nix
+++ home-manager/i3.nix
@@ -1,6 +1,12 @@
 { config, lib, pkgs, ... }:
 
-xsession.scriptPath = ".hm-session";
-xsession.windowManager.i3 = {
-  enable = true;
+{
+  xsession.scriptPath = ".hm-session";
+  xsession.windowManager.i3 = {
+    enable = true;
+
+    config = {
+      modifier = "Mod4";
+    };
+  };
 }
