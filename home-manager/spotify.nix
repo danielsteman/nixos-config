287175e20a49a18bc3c696bf9c79ee880d2c5f80
Daniel Steman <daniel-steman@live.nl>
2023-04-26 23:37:03 +0200

    getting spotifyd to work

--- home-manager/spotify.nix
+++ home-manager/spotify.nix
@@ -3,5 +3,12 @@
 {
   services.spotifyd = {
     enable = true;
+    settings = {
+      username = "116885657";
+      backend = "pulseaudio";
+      device = "pipewire";
+      deviceName = "spotifyd";
+      bitrate = 320;
+    };
   };
 }
