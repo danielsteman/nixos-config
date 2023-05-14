3c2753ea677656305931091746a48ec6148726ad
Daniel Steman <daniel-steman@live.nl>
2023-05-14 15:56:53 +0200

    install vscode without ext

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -3,10 +3,5 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-    ];
   };
 }
